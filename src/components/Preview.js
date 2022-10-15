2c3989ddef613c06f0ab6aae3350e11215d7bdf5
ALOK KUMAR <107565523+alok0466@users.noreply.github.com>
2022-10-15 11:09:22 +0530

    preview has finished

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useState } from "react";
 export default function Preview() {
+  // here we are going to fetch quiz by localStorage
+
   let showQuiz = () => {
     let val = localStorage.getItem("Quizes");
     val = JSON.parse(val);
     let id = localStorage.getItem("QuizId");
-
+    console.log(val);
     let quizid;
     let quiztitle;
     let quizz;
@@ -22,31 +24,93 @@ export default function Preview() {
         break;
       }
     }
+    console.log(quizz);
+    let alok = "";
+    let QuestionCount;
 
-    let alok ; 
-
-    for ( let x = 0 ; x<quizz.length; x++)
-    {
-      let currentItem = quizz[x]
+    for (let x = 0; x < quizz.length; x++) {
+      let currentItem = quizz[x];
       let currentQuestion = currentItem.Question;
-      let optionA = currentItem.OptionA
-      let optionB = currentItem.OptionB
-      let optionC = currentItem.OptionC
-      let optionD = currentItem.OptionD
+      console.log(currentQuestion);
+      let optionA = currentItem.OptionA;
+      let optionB = currentItem.OptionB;
+      let optionC = currentItem.OptionC;
+      let optionD = currentItem.OptionD;
 
-      alok+= `
-        ${currentQuestion} 
-      <br>
-  
-       ${optionA}
-  ${optionB}
-    ${optionC}
-     ${optionD}
+      alok += `
+                 
 
-      `
+      <div className="container">
+      <h1 className="text-center">
+   ${quiztitle}
+      </h1>
+      <br />
+      <p>
+        <b>
+          
+         Question ${x+1}
+           - ${currentQuestion}
+        </b>
+      </p>
+      <div className="container">
+        <div className="">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault1">
+          ${optionA}
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault2"
+            defaultChecked=""
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault2">
+          ${optionB}
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault1">
+          ${optionC}
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault2"
+            defaultChecked=""
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault2">
+          ${optionD}
+          </label>
+        </div>
+
+      
+      </div>
+    </div>
+    
+    
+      `;
+      setQuestionCount(x + 1);
     }
-
- 
+    console.log(alok);
+    let main = document.getElementById("preview");
+    main.innerHTML = alok;
     setcontent(alok);
     setqid(quizid);
     settitle(quiztitle);
@@ -55,17 +119,15 @@ export default function Preview() {
   const [content, setcontent] = useState();
   const [title, settitle] = useState();
   const [qid, setqid] = useState();
+  const [QuestionCount, setQuestionCount] = useState();
   return (
     <div>
       <button className="btn btn-success my-3" onClick={showQuiz}>
         Show Quiz
       </button>
       <p className="quizid">{qid}</p>
-      <h1 id="QuizTitle">{title}</h1>
-      <h3 id="question">
-        {" "}
-        Q{}- {content}{" "}
-      </h3>
+    
+      <div id="preview"></div>
     </div>
   );
 }
